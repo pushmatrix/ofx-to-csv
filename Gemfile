872c28a80e4ad57db0ec48c3aa694819baba035d
Daniel Beauchamp <daniel.beauchamp@shopify.com>
2011-11-01 11:32:13 -0400

    Handle RBC's format.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source :rubygems
 
-gem 'ofx-parser'
+gem 'ofx-parser', '0.1.0', :git => "git://github.com/pushmatrix/ofx-parser.git"
 gem 'fastercsv'
+gem 'hpricot'
 
 # Manually require i18n because ActiveSupport, for whatever reason, doesn't require it
 gem 'i18n'
--- ofx-to-csv.rb
+++ ofx-to-csv.rb
@@ -8,18 +8,29 @@ require 'fastercsv'
 require 'active_support/inflector'
 
 ofx_file = ARGV.shift
+
+
 unless ofx_file and File.exists?(ofx_file)
   puts "Usage: #{File.basename(__FILE__)} /path/to/statement.ofx"
   exit 1
 end
 
-ofx = OfxParser::OfxParser.parse(open(ofx_file))
+# RBC does not specify the timezone string.
+# Add it in
+contents = File.read(ofx_file)
+contents = contents.gsub('[-5]', '[-5:EST]')
 
-ofx.bank_account.statement.transactions.each do |transaction|
-  date  = transaction.date.strftime('%Y-%m-%d')
-  payee = ActiveSupport::Inflector.titleize(transaction.payee)
-  memo  = ActiveSupport::Inflector.titleize(transaction.memo)
-  type  = ActiveSupport::Inflector.titleize(transaction.type)
-  
-  puts [date, transaction.fit_id, payee, memo, type, transaction.amount].to_csv
+
+ofx = OfxParser::OfxParser.parse(contents)
+
+
+ofx.credit_cards.each do |credit_card|
+  credit_card.statement.transactions.each do |transaction|
+    date  = transaction.date.strftime('%Y-%m-%d')
+    payee = ActiveSupport::Inflector.titleize(transaction.payee)
+    memo  = ActiveSupport::Inflector.titleize(transaction.memo)
+    type  = ActiveSupport::Inflector.titleize(transaction.type)
+    puts [credit_card.number,date, transaction.fit_id, payee, transaction.amount].to_csv
+  end
+
 end
